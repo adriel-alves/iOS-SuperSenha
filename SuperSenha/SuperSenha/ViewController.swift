4e74aa465496e479d84ca5f1674bc5e05607734a
Adriel Alves <adriel@MacBook-Pro-de-pc.local>
2018-12-07 16:04:54 -0200

    iniciando app

---
+++ SuperSenha/SuperSenha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Adriel Alves on 07/12/18.
+//  Copyright © 2018 Adriel Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
